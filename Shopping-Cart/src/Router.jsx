da11a6cdcc864ebcc31cfc9aa6a3bd7033f77576
Nagul75 <nagulk697@gmail.com>
2025-02-18 22:08:58 +0530

    test

---
+++ Shopping-Cart/src/Router.jsx
@@ -0,0 +1,10 @@
+import Home from "./Components/Home";
+
+const router = [
+    {
+        path: "/",
+        element: <Home/>
+    }
+]
+
+export default router
--- Shopping-Cart/src/main.jsx
+++ Shopping-Cart/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import router from './Router'
+const routes = createBrowserRouter(router)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
