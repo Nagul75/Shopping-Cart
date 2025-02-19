571bda182d876a1635db1de436119ee3e50b0916
Nagul75 <nagulk697@gmail.com>
2025-02-19 23:02:32 +0530

    display content in card

--- Shopping-Cart/src/Components/Card.jsx
+++ Shopping-Cart/src/Components/Card.jsx
@@ -1,10 +1,20 @@
 import "../Styles/Card.css"
 
-export default function Card({children}) {
+export default function Card({product}) {
+    console.log(product)
     return(
         <>
                 <div className="card">
-                    {children}
+                    <div className="image">
+                        <img src={product.image} alt={product.title} />
+                    </div>
+                    <div className="product-title">
+                        <p>{product.title}</p>
+                    </div>
+                    <div className="price-rating">
+                        <p>{product.price}$</p>
+                        <p>{product.rating.rate}/5 <i>{product.rating.count}</i></p>
+                    </div>
                 </div>
         </>
     )
--- Shopping-Cart/src/Components/Products.jsx
+++ Shopping-Cart/src/Components/Products.jsx
@@ -1,17 +1,25 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Card from "./Card"
 import "../Styles/Products.css"
 
 export default function Products() {
 
+    const [products, setProducts] = useState([])
+
+    useEffect(()=> {
+        fetch('https://fakestoreapi.com/products?limit=18')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .catch(err=>console.error(err))
+    }, [])
+
+
     return(
         <>
             <div className="card-container">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+                {products.map(product =>
+                    <Card product = {product}/>
+                 )}
             </div>
         </>
     )
