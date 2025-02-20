784c9c3bd5663b92624e798c1ff084f9a33293cf
Nagul75 <nagulk697@gmail.com>
2025-02-20 13:12:07 +0530

    Update Cart.jsx: Add Total

--- Shopping-Cart/src/Components/Cart.jsx
+++ Shopping-Cart/src/Components/Cart.jsx
@@ -1,13 +1,23 @@
 import CartItem from "./CartItem"
 import "../Styles/Cart.css"
+import { useOutletContext } from "react-router-dom"
 
 export default function Cart() {
+    const [cart, setCart] = useOutletContext()
+    let total = 0
+    cart.forEach(product => {
+        total += product.price
+    });
+
     return(
         <>
             <div className="cart-container">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {cart.map(item =>
+                    <CartItem product = {item}/>
+                )}
+            </div>
+            <div className="total-price">
+                <h1>{total}</h1>
             </div>
         </>
     )
--- Shopping-Cart/src/Components/CartItem.jsx
+++ Shopping-Cart/src/Components/CartItem.jsx
@@ -1,9 +1,9 @@
 import "../Styles/CartItem.css"
 
-export default function CartItem() {
+export default function CartItem({product}) {
     return (
         <div className="cart-item">
-
+            <h1>{product.title}</h1>
         </div>
     )
 }
